8e381cd90f6a2dc709fc37b599211a6936c20b32
Vinicius de Luca <116411691+LucaAnalyzes@users.noreply.github.com>
2023-02-07 15:37:12 -0300

    teste2

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ const isPhantomInstalled = window.phantom?.solana?.isPhantom
     const connect = async () => {
       const provider = getProvider(); // see "Detecting the Provider"
       try {
+        console.log(walletAddress());
         const resp = await provider.request({method:"connect"});
-        console.log(resp.publicKey.toString());
+        console.log(walletAddress());
         var AddrressWallet= resp.publicKey.toString();
         // 26qv4GCcx98RihuK3c4T6ozB3J7L6VwCuFVc7Ta2A3Uo
       } catch (err) {
@@ -23,7 +24,15 @@ const isPhantomInstalled = window.phantom?.solana?.isPhantom
       }
     };
     
-    
+    const walletAddress = () => {
+        const provider = getProvider();
+        try {
+            return provider.publicKey.toBase58();
+        } catch (err) {
+            return '';
+        }
+    }
+
     setTimeout(()=> {connect()},2000);
   
     
